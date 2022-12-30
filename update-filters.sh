bc232a2dfba9105112285bb356ab59b450f7e011
luxysiv <46205571+luxysiv@users.noreply.github.com>
2022-12-30 12:53:48 +0700

    Update update-filters.sh

--- update-filters.sh
+++ update-filters.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
 wget -O filters/annoyances.txt https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt
-
 wget -O filters/social.txt https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_4_Social/filter.txt
-
 wget -O filters/spyware.txt https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt
-
 wget -O filters/track-param.txt https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_17_TrackParam/filter.txt
-
 wget -O filters/vietnamese.txt https://github.com/luxysiv/Blockads/raw/master/Android/abpvn-ext.txt
 
 filters=($(ls -S filters/*))
